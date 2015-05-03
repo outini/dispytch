850833d2c3ef639d884d2928e8f3f466cc1ef156
Denis 'jawa' Pompilio <denis.pompilio@gmail.com>
2015-05-03 23:00:32 +0200

    Script test.sh modified for tests purposes

--- bin/test.sh
+++ bin/test.sh
@@ -10,7 +10,16 @@
 # If not defined, dispatcher will assume commandline call
 
 export REQUEST_METHOD="GET"
-export REQUEST_URI="/munin/by-ip/1.1.1.1/cpu/AVERAGE/now-2h/now"
 
-# It is assumed you runned this script from the repository root
+export DOCUMENT_PATH="/munin/by-ip/1.1.1.1/cpu/AVERAGE/now-2w/now"
+export DOCUMENT_PATH="/munin/by-id/clientA;test;hostA/cpu/AVERAGE/now-2w/now"
+export REQUEST_URI="$DOCUMENT_PATH"
 python ./dispytch/__init__.py
+
+#export DOCUMENT_PATH="/munin/list"
+#export REQUEST_URI="/munin/list"
+#python ./dispytch/__init__.py
+
+#export DOCUMENT_PATH="/info"
+#export REQUEST_URI="/info"
+#python ./dispytch/__init__.py
